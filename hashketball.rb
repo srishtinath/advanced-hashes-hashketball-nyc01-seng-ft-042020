e8a53411fd3fd47478c699dc900d2a485dfb9da5
Srishti Nath <srishti.nath@gmail.com>
2020-04-14 18:47:38 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -131,7 +131,7 @@ end
 def num_points_scored(player_name_input)
   game_hash.each do |team, attribute|
     attribute.each do |attribute_value, player_array|
-      if aplayer_array.is_a?(String) == false
+      if player_array.is_a?(String) == false
           player_array.each do |player_stats|
           if player_stats[:player_name] == player_name_input
             return player_stats[:points]
